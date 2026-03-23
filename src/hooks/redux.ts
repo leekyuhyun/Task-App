e64436993f5c51800cdafb3f84a1475d11623203
leekyuhyun <leekh010502@gmail.com>
2026-03-23 22:49:29 +0900

    chore : base setting

---
+++ src/hooks/redux.ts
@@ -0,0 +1,5 @@
+import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux';
+import type { AppDispatch, RootState } from '../store';
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
