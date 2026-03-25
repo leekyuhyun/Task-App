8d6564bcbcf2e7f0436757165e7969552b45252c
leekyuhyun <leekh010502@gmail.com>
2026-03-25 18:45:05 +0900

    feat : ListsContainer 컴포넌트 작업

--- src/components/ListsContainer/ListsContainer.tsx
+++ src/components/ListsContainer/ListsContainer.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import type { IList } from '../../types';
+import type { FC } from 'react';
+import { List } from '../List/List';
+import { ActionButton } from '../ActionButton/ActionButton';
+import { listContainerStyles } from './ListsContainer.css';
 
-const ListsContainer = () => {
-  return <div>ListsContainer</div>;
+type TListContainerProps = {
+  boardId: string;
+  lists: IList[];
 };
 
-export default ListsContainer;
+export const ListsContainer: FC<TListContainerProps> = ({ boardId, lists }) => {
+  return (
+    <div className={listContainerStyles}>
+      {lists.map((list) => (
+        <List key={list.listId} list={list} boardId={boardId} />
+      ))}
+      <ActionButton boardId={boardId} listId={''} list />
+    </div>
+  );
+};
