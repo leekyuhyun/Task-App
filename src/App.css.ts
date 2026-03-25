fe595ec4bb367ea6df11a130312a3cb3ab3d849b
leekyuhyun <leekh010502@gmail.com>
2026-03-25 18:49:10 +0900

    feat : App 컴포넌트 작업

--- src/App.css.ts
+++ src/App.css.ts
@@ -5,19 +5,19 @@ export const vars = createGlobalTheme(':root', {
     main: '#ffa726',
     mainDarker: '#f57c00',
     mainFaded: '#ffb74d',
-    mainFadedBright: '#ffb74da6',
-    list: 'rgba(255, 236, 240)',
-    task: 'rgba(255, 255, 255)',
-    taskHover: 'rgba(245,245,245)',
-    brightText: 'rgba(255, 255, 255)',
-    darkText: 'rgba(24,42,77)',
-    secondaryDarkText: 'rgba(94,108,132)',
-    secondaryDarkTextHover: 'rgba(218,219,226)',
-    selectedTab: 'rgb(137,176,174)',
-    updateButton: 'rgb(237,180,88)',
-    deleteButton: 'rgb(237,51,88)',
+    mainFededBright: '#ffb74da6',
+    list: 'rgb(235, 236, 240)',
+    task: 'rgb(255, 255, 255)',
+    taskHover: 'rgb(245, 245, 245)',
+    brightText: 'rgb(255, 255, 255)',
+    darkText: 'rgb(24, 42, 77)',
+    secondaryDarkText: 'rgb(94, 108, 132)',
+    secondaryDarkTextHover: 'rgb(218, 219, 226)',
+    selectedTab: 'rgb(137, 176, 174)',
+    updateButton: 'rgb(237, 180, 88)',
+    deleteButton: 'rgb(237, 51, 88)',
   },
-  fontSize: {
+  fontSizing: {
     T1: '32px',
     T2: '24px',
     T3: '18px',
@@ -32,10 +32,10 @@ export const vars = createGlobalTheme(':root', {
     listSpacing: '30px',
   },
   font: {
-    body: 'aria',
+    body: 'arial',
   },
   shadow: {
-    basic: '4px 4px 8px 0px  rgba(34,60, 80)',
+    basic: '4px 4px 5px 0px rgb(34, 60, 80, 0.2)',
   },
   minWidth: {
     list: '250px',
@@ -47,7 +47,7 @@ export const appContainer = style({
   flexDirection: 'column',
   minHeight: '100vh',
   height: 'max-content',
-  width: '100%',
+  width: '100vw',
 });
 
 export const board = style({
@@ -59,4 +59,40 @@ export const board = style({
 export const buttons = style({
   marginTop: 'auto',
   paddingLeft: vars.spacing.big2,
+});
+
+export const deleteBoardButton = style({
+  border: 'none',
+  borderRadius: 5,
+  width: 'max-content',
+  marginTop: 'auto',
+  marginLeft: 'auto',
+  marginBottom: 30,
+  fontSize: vars.fontSizing.T4,
+  padding: vars.spacing.big2,
+  backgroundColor: vars.color.mainFaded,
+  cursor: 'pointer',
+  opacity: 0.6,
+  minWidth: 150,
+  ':hover': {
+    opacity: 0.8,
+  },
+});
+
+export const loggerButton = style({
+  border: 'none',
+  borderRadius: 5,
+  width: 'max-content',
+  marginTop: 'auto',
+  marginLeft: '15px',
+  marginBottom: '30px',
+  fontSize: vars.fontSizing.T4,
+  padding: vars.spacing.big2,
+  backgroundColor: vars.color.mainFaded,
+  cursor: 'pointer',
+  opacity: 0.6,
+  minWidth: 150,
+  ':hover': {
+    opacity: 0.8,
+  },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,73 @@
-import { appContainer, board, buttons } from './App.css';
+import { useState } from 'react';
+import { appContainer, buttons, board, deleteBoardButton, loggerButton } from './App.css';
+import { BoardList } from './components/BoardList/BoardList';
+import { ListsContainer } from './components/ListsContainer/ListsContainer';
+import { useTypedDispatch, useTypedSelector } from './hooks/redux';
+import { EditModal } from './components/EditModal/EditModal';
+import { LoggerModal } from './components/LoggerModal/LoggerModal';
+import { deleteBoard } from './store/slices/boardsSlice';
+import { addLog } from './store/slices/loggerSlice';
+import { v4 } from 'uuid';
 
 function App() {
+  const dispatch = useTypedDispatch();
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
+  const [activeBoardId, setActiveBoardId] = useState('board-0');
+  const modalActive = useTypedSelector((state) => state.boards.modalActive);
+  const boards = useTypedSelector((state) => state.boards.boardArray);
+  const getActiveBoard = boards.filter((board) => board.boardId === activeBoardId)[0];
+  const lists = getActiveBoard.lists;
+
+  const handleDeleteBoard = () => {
+    if (boards.length > 1) {
+      dispatch(deleteBoard({ boardId: getActiveBoard.boardId }));
+
+      dispatch(
+        addLog({
+          logId: v4(),
+          logMessage: `게시판 "${getActiveBoard.boardName}"이(가) 삭제되었습니다.`,
+          logAuthor: 'User',
+          logTimestamp: String(Date.now()),
+        }),
+      );
+
+      const newIndexToSet = () => {
+        const indexToBeDeleted = boards.findIndex((board) => board.boardId === activeBoardId);
+
+        return indexToBeDeleted === 0 ? indexToBeDeleted + 1 : indexToBeDeleted - 1;
+      };
+
+      setActiveBoardId(boards[newIndexToSet()].boardId);
+    } else {
+      alert('최소 게시판 개수는 1개입니다.');
+    }
+  };
+
   return (
-    <div className={appContainer}>
-      <div className={board}></div>
-      <div className={buttons}>
-        <button>이 게시판 삭제하기</button>
-        <button>이 게시판 수정하기</button>
+    <>
+      <div className={appContainer}>
+        {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
+
+        {modalActive ? <EditModal /> : null}
+
+        <div>
+          <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId} />
+        </div>
+
+        <div className={board}>
+          <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+        </div>
+
+        <div className={buttons}>
+          <button onClick={handleDeleteBoard} className={deleteBoardButton}>
+            게시판 삭제하기
+          </button>
+          <button onClick={() => setIsLoggerOpen(true)} className={loggerButton}>
+            {isLoggerOpen ? '활동 목록 숨기기' : '활동 목록 보이기'}
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
