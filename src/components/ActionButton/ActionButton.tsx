0c8293d3609c6fee4537d96f6a8ffee4e2769a68
leekyuhyun <leekh010502@gmail.com>
2026-03-25 18:43:41 +0900

    feat : ActionButton 컴포넌트 작업

--- src/components/ActionButton/ActionButton.tsx
+++ src/components/ActionButton/ActionButton.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import type { FC } from 'react';
+import { useState } from 'react';
+import { DropDownForm } from './DropDownForm/DropDownForm';
+import { IoIosAdd } from 'react-icons/io';
+import { listButton, taskButton } from './ActionButton.css';
 
-const ActionButton = () => {
-  return <div>ActionButton</div>;
+type TActionButtonProps = {
+  boardId: string;
+  listId: string;
+  list?: boolean;
 };
 
-export default ActionButton;
+export const ActionButton: FC<TActionButtonProps> = ({
+  boardId,
+  listId,
+  list,
+}: TActionButtonProps) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const buttonText = list ? '새로운 리스트 등록' : '새로운 태스크 등록';
+
+  return isFormOpen ? (
+    <DropDownForm
+      setIsFormOpen={setIsFormOpen}
+      list={list ? true : false}
+      boardId={boardId}
+      listId={listId}
+    />
+  ) : (
+    <div className={list ? listButton : taskButton} onClick={() => setIsFormOpen(true)}>
+      <IoIosAdd />
+      <p>{buttonText}</p>
+    </div>
+  );
+};
