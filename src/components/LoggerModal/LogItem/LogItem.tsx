fc96e1d80570adb25e6da4dc5c41608435a7b601
leekyuhyun <leekh010502@gmail.com>
2026-03-25 18:45:31 +0900

    feat : LogItem 컴포넌트 작업

--- src/components/LoggerModal/LogItem/LogItem.tsx
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import type { ILogItem } from '../../../types';
+import type { FC } from 'react';
+import { useState } from 'react';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { item, author, date, message } from './LogItem.css';
 
-const LogItem = () => {
-  return <div>LogItem</div>;
+type TLogItemProps = {
+  logItem: ILogItem;
 };
 
-export default LogItem;
+export const LogItem: FC<TLogItemProps> = ({ logItem }) => {
+  const [timeOffSet] = useState(() => new Date(Date.now() - Number(logItem.logTimestamp)));
+
+  const showOffsetTime = `
+    ${timeOffSet.getMinutes() > 0 ? `${timeOffSet.getMinutes()}m` : ''}
+    ${timeOffSet.getSeconds() > 0 ? `${timeOffSet.getSeconds()}s ago` : ''}
+    ${timeOffSet.getSeconds() === 0 ? `just now` : ''}
+  `;
+
+  return (
+    <div className={item}>
+      <div className={author}>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div className={message}>{logItem.logMessage}</div>
+      <div className={date}>{showOffsetTime}</div>
+    </div>
+  );
+};
