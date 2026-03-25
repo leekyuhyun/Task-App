1d346816bdb721660254291d3a6698d3abe9800e
leekyuhyun <leekh010502@gmail.com>
2026-03-25 18:51:13 +0900

    fix : main 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDom from 'react-dom/client';
-import App from './App';
-import './index.css';
 import { Provider } from 'react-redux';
-import store from './store';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import store from './store/index.ts';
 
-ReactDom.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
   </Provider>,
