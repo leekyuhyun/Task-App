6e80869f204176d622bd3aaa5df2593614e31863
leekyuhyun <leekh010502@gmail.com>
2026-03-25 18:44:47 +0900

    feat : List 컴포넌트 작업

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import { GrSubtract } from 'react-icons/gr';
+import { Task } from '../Task/Task';
+import { ActionButton } from '../ActionButton/ActionButton';
+import { v4 } from 'uuid';
+import type { FC } from 'react';
+import type { IList, ITask } from '../../types';
+import { useTypedDispatch } from '../../hooks/redux';
+import { deleteList, setModalActive } from '../../store/slices/boardsSlice';
+import { addLog } from '../../store/slices/loggerSlice';
+import { setModalData } from '../../store/slices/modalSlice';
+import { listWrapper, header, name, deleteButton } from './List.css';
 
-const List = () => {
-  return <div>List</div>;
+type TListProps = {
+  boardId: string;
+  list: IList;
 };
 
-export default List;
+export const List: FC<TListProps> = ({ list, boardId }) => {
+  const dispatch = useTypedDispatch();
+  const handleListDelete = (listId: string) => {
+    dispatch(deleteList({ boardId, listId }));
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `List ${list.listName} deleted`,
+        logAuthor: 'User',
+        logTimestamp: String(Date.now()),
+      }),
+    );
+  };
+  const handleTaskChange = (boardId: string, listId: string, task: ITask) => {
+    dispatch(setModalData({ boardId, listId, task }));
+    dispatch(setModalActive(true));
+  };
+
+  return (
+    <div className={listWrapper}>
+      <div className={header}>
+        <div className={name}>{list.listName}</div>
+        <GrSubtract onClick={() => handleListDelete(list.listId)} className={deleteButton} />
+      </div>
+
+      <div>
+        {list.tasks.map((task, index) => (
+          <div onClick={() => handleTaskChange(boardId, list.listId, task)} key={task.taskId}>
+            <Task
+              taskName={task.taskName}
+              taskDescription={task.taskDescription}
+              boardId={boardId}
+              id={task.taskId}
+              index={index}
+            />
+          </div>
+        ))}
+      </div>
+      <ActionButton boardId={boardId} listId={list.listId} />
+    </div>
+  );
+};
